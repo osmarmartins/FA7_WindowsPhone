c351b007e48a57ceddf3f4bcd9fa9bf5f2e231f2
osmar de oliveira martins filho <1581444>
2016-05-21 15:42:34 -0300

    Exercicio PATH

---
+++ AULA02/ExemploLocalization/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ExemploLocalization.Resources;
+
+namespace ExemploLocalization
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ AULA02/PathApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PathApp.Resources;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace PathApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Point oldPoint;
+        Point currentPoint;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            canvas.MouseMove += Canvas_MouseMove;
+            canvas.MouseLeftButtonDown += Canvas_MouseLeftButtonDown;
+
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            currentPoint = e.GetPosition(canvas);
+            oldPoint = e.GetPosition(canvas);
+        }
+
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            currentPoint = e.GetPosition(canvas);
+            Line linha = new Line();
+            linha.X1 = currentPoint.X;
+            linha.Y1 = currentPoint.Y;
+            linha.X2 = oldPoint.X;
+            linha.Y2 = oldPoint.Y;
+
+            linha.Stroke = new SolidColorBrush(Colors.Blue);
+            linha.StrokeThickness = 5;
+            canvas.Children.Add(linha);
+
+            oldPoint = currentPoint;
+
+        }
+    }
+}
